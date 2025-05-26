a3b0e0029bb74fcaa2e291b2dfca4f58d22d8efd
“Miguel <miguel.mendeza@vml.com>
2025-05-26 15:51:29 -0600

    index creation

---
+++ opti-sass/src/components/cms/page/BlankExperience/index.tsx
@@ -0,0 +1,20 @@
+import { type OptimizelyNextPage as CmsComponent } from "@remkoj/optimizely-cms-nextjs";
+import { BlankExperiencePageDataFragmentDoc, type BlankExperiencePageDataFragment } from "@/gql/graphql";
+    const componentName = 'BlankExperience'
+    const componentInfo = ''
+    return <div className="mx-auto px-2 container">
+        <div className="font-bold italic">{ componentName }</div>
+        <div>{ componentInfo }</div>
+        { Object.getOwnPropertyNames(data).length > 0 && <pre className="w-full overflow-x-hidden font-mono text-sm bg-slate-200 p-2 rounded-sm border border-solid border-slate-900 text-slate-900">{ JSON.stringify(data, undefined, 4) }</pre> }
+        { children && <div className="flex flex-col mt-4 mx-4">{ children }</div>}
+    </div>
+}
+BlankExperience.displayName = "BlankExperience (Page/BlankExperience)"
+BlankExperience.getDataFragment = () => ['BlankExperienceData', BlankExperienceDataFragmentDoc]
+BlankExperience.getMetaData = async (contentLink, locale, client) => {
+    const sdk = getSdk(client);
+    // Add your metadata logic here
+    return {}
+}
+
+export default BlankExperience
